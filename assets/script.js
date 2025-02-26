0f8e7799891503d425bed39c4b16b6616c610e21
Arthur Barros <mb.arthurmiguel@gmail.com>
2025-02-26 10:57:38 -0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
+// Obtendo elementos HTML
 const ageBlitter = document.querySelector('p#age-blitter')
 const ageInput = document.querySelector('input#age-input')
 
+// Função que atualiza o exibidor de valor do input do tipo range
 function updateAgeBlitter() {
     ageBlitter.textContent = ageInput.value
     ageBlitter.style.transform = `translateX(${ageInput.value * 1.6}px)`
